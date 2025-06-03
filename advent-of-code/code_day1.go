adc6b857946818e1251cd9618dd06809849bbb31
Ashish Verma <ashv6561@gmail.com>
2025-06-03 16:11:23 +0530

    Advent of Code : Golang Day 1

---
+++ advent-of-code/code_day1.go
@@ -0,0 +1,57 @@
+package adventofcode
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+func adventDay1() {
+	dat, err := os.ReadFile("input_day1.txt")
+
+	if err != nil {
+		panic(err)
+	}
+	inputs := strings.Split(string(dat), "\n")
+	count := 0
+	Lefts := make([]int, 0)
+	Rights := make([]int, 0)
+	freq := make(map[int]int, 0)
+
+	for ind, v := range inputs {
+		// Split into left and right list
+		row := strings.Fields(v)
+		left, l_err := strconv.Atoi(row[0])
+		right, r_err := strconv.Atoi(row[1])
+		if l_err != nil || r_err != nil {
+			fmt.Printf("Error Occured on Line : %d\n", ind)
+			return
+		}
+		Lefts = append(Lefts, left)
+		Rights = append(Rights, right)
+		val, ok := freq[right]; if ok {
+			freq[right] = val + 1
+		} else {
+			freq[right] = 1
+		}
+	}
+	sort.Ints(Lefts)
+	sort.Ints(Rights)
+	similarity := 0
+	
+	for ind, _ := range Lefts {
+		fmt.Printf("Index %d : (%d, %d)\n", ind, Lefts[ind], Rights[ind])
+		if Lefts[ind] > Rights[ind] {
+			count += Lefts[ind] - Rights[ind]
+		} else {
+			count += Rights[ind] - Lefts[ind]
+		}
+		val, ok := freq[Lefts[ind]]; if ok {
+			similarity += val * Lefts[ind]
+		}
+	}
+
+	fmt.Println("Total Count :", count)
+	fmt.Println("Similarity  :", similarity)
+}
